45c6e883fad61410232ce5ef0c86c94e2c53942e
mathias hansen <mathiasosthan@gmail.com>
2022-06-27 15:47:34 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const stream = require("stream");
+
+const submodulePath = core.getInput("submodule-path");
+const requiredVersion = core.getInput("version");
+
+async function execToString(command, args, options) {
+    let output = "";
+    const outStream = new stream.PassThrough();
+    outStream.on("data", c => output += c.toString());
+
+    const exitCode = await exec.exec(command, args, options);
+
+    if (exitCode > 0) {
+        throw new Error(output);
+    }
+
+    return output;
+}
+
+async function getVersion(ref) {
+    const submoduleVersion = await execToString("git", ["rev-parse", ref], {
+        cwd: submodulePath,
+        outStream,
+    });
+
+    return submoduleVersion;
+}
+
+async function run() {   
+    const localVersion = await getVersion("HEAD");
+    const remoteVersion = await getVersion(requiredVersion);
+
+    if (localVersion != remoteVersion) {
+        throw new Error("The remote version does not match the local version of submodule: " + submodulePath);
+    }
+}
+
+(async () => {
+    try {
+        await run();
+    } catch (e) {
+        core.setFailed(e);
+    }
+})();
